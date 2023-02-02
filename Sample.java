ca49c09e2da2b6ce66eaa8a5e9d26e6e794db3b2
Suruthy Selvakumar <92316139+suruthyselvakumar@users.noreply.github.com>
2023-02-02 15:50:22 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,5 +1,6 @@
 public class HelloGuys{
-public static void main(String[] args){
+public static void main(String[] args)
+{
 System.out.println("Hey Shruthi");
 System.out.println("Hey Kavitha");
 System.out.println("Hey Twinkie");
