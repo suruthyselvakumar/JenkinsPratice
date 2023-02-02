f00faa5b3436389f3162931709c7fe3b172d6932
Suruthy Selvakumar <92316139+suruthyselvakumar@users.noreply.github.com>
2023-02-02 14:22:06 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,8 @@
+public class HelloGuys{
+public static void main(String[] args){
+System.out.println("Hey Shruthi");
+System.out.println("Hey Kavitha");
+System.out.println("Hey Twinkie");
+System.out.println("Hey Taffie");
+}
+}
