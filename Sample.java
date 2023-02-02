cfc30f99c51789a8be1f8450c0ad1295d4810c01
Suruthy Selvakumar <92316139+suruthyselvakumar@users.noreply.github.com>
2023-02-02 16:31:41 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,6 +5,7 @@ System.out.println("Hey Shruthi");
 System.out.println("Hey Kavitha");
 System.out.println("Hey Twinkie");
 System.out.println("Hey Taffie");
+  System.out.println("Hey vijay");
   
   
 }
